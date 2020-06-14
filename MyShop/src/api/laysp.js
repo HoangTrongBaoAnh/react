6d12133e1b3ad563cac303a35fa2d116da025239
HoangAnh <biminhiabo2@gmail.com>
2020-06-14 23:08:28 +0700

    .

--- MyShop/src/api/laysp.js
+++ MyShop/src/api/laysp.js
@@ -1,5 +1,5 @@
 const laydssanpham = (idType, page) => {
-    return fetch(`http://192.168.100.17/app/product_by_type.php?id_type=${idType}&page=${page}`)
+    return fetch(`http://192.168.43.28/app/product_by_type.php?id_type=${idType}&page=${page}`)
     .then(res => res.json());
 }
 
